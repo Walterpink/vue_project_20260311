57dc4ab4bc373fddac83a09dd91d68a543ea2eeb
walter white <walter20201111@163.com>
2026-03-11 21:22:50 +0800

    feat(watch监视):情况二：监视【ref】定义的【对象类型】数据,情况三：监视【reactive】定义的【对象类型】数据

---
+++ backups/013_watch监视_情况一/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+        <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">点我sum+1</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person',
+}
+</script>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+let sum = ref(0);
+
+function changeSum() {
+    sum.value = sum.value + 1;
+}
+
+//监视，情况一：监视ref定义的基本类型数据
+const stopWatch = watch(sum, (newValue, oldValue) => {
+
+    console.log('sum值改变了', newValue, oldValue);
+    if (newValue > 10) {
+        stopWatch();
+        console.log('停止监视');
+    }
+});
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+
+li {
+    font-size: 20px;
+}
+</style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="person">
-        <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
-        <h2>当前求和为：{{ sum }}</h2>
-        <button @click="changeSum">点我sum+1</button>
+        <h1>情况三：监视【reactive】定义的【对象类型】数据</h1>
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changePerson">修改整个人</button>
+        <hr>
+        <h2>测试：{{ obj.a.b.c }}</h2>
+        <button @click="test">修改obj.a.b.c</button>
     </div>
 </template>
 
@@ -13,22 +19,42 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-let sum = ref(0);
-
-function changeSum() {
-    sum.value = sum.value + 1;
+import { reactive, watch } from 'vue';
+// 数据
+let person = reactive({
+    name: '张三',
+    age: 18
+})
+let obj = reactive({
+    a: {
+        b: {
+            c: 666
+        }
+    }
+})
+// 方法
+function changeName() {
+    person.name += '~'
+}
+function changeAge() {
+    person.age += 1
+}
+function changePerson() {
+    Object.assign(person, { name: '李四', age: 80 })
+}
+function test() {
+    obj.a.b.c = 888
 }
 
-//监视，情况一：监视ref定义的基本类型数据
-const stopWatch = watch(sum, (newValue, oldValue) => {
+// 监视，情况三：监视【reactive】定义的【对象类型】数据，且默认是开启深度监视的
+watch(person, (newValue, oldValue) => {
+    console.log('person变化了', newValue, oldValue)
+})
+watch(obj, (newValue, oldValue) => {
+    console.log('Obj变化了', newValue, oldValue)
+})
 
-    console.log('sum值改变了', newValue, oldValue);
-    if (newValue > 10) {
-        stopWatch();
-        console.log('停止监视');
-    }
-});
+
 </script>
 
 <style scoped>
