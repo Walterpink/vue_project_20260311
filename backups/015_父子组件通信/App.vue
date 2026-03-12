4169baec3c309541538ee7d0534fd2a1a21d3e59
walter white <walter20201111@163.com>
2026-03-12 10:29:44 +0800

    feat(slot插槽):上传代码

---
+++ backups/015_父子组件通信/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <UserCard
+      v-for="user in users"
+      :key="user.id"
+      :user="user"
+      @save="handleSave"
+      @delete="handleDelete"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import UserCard from './components/UserCard.vue'
+
+const users = ref([
+  { id: 1, name: '张三', age: 28 },
+  { id: 2, name: '李四', age: 32 }
+])
+
+const handleSave = ({ id, name }: { id: number; name: string }) => {
+  const user = users.value.find(u => u.id === id)
+  if (user) user.name = name
+}
+
+const handleDelete = (id: number) => {
+  users.value = users.value.filter(u => u.id !== id)
+}
+</script>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <div>
-    <UserCard
-      v-for="user in users"
-      :key="user.id"
-      :user="user"
-      @save="handleSave"
-      @delete="handleDelete"
-    />
+    <FancyButton @click="handleClick">Click me</FancyButton>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import UserCard from './components/UserCard.vue'
+import FancyButton from './components/FancyButton.vue';
 
-const users = ref([
-  { id: 1, name: '张三', age: 28 },
-  { id: 2, name: '李四', age: 32 }
-])
-
-const handleSave = ({ id, name }: { id: number; name: string }) => {
-  const user = users.value.find(u => u.id === id)
-  if (user) user.name = name
-}
-
-const handleDelete = (id: number) => {
-  users.value = users.value.filter(u => u.id !== id)
+function handleClick() {
+  console.log('Button clicked!');
 }
 </script>
