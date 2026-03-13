de9a3794347e6f2a08e64a2e86ac7815922ac30e
walter white <walter20201111@163.com>
2026-03-13 10:27:01 +0800

    style(): 备份代码

---
+++ backups/016_slot&组件通信/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+import Publisher from './components/Publisher.vue';
+import Subscriber from './components/Subscriber.vue';
+</script>
+
+<template>
+  <MyComponent v-slot="{ text, count }">
+    {{ text }} {{ count }}
+  </MyComponent>
+  <br />
+  <Publisher />
+  <br />
+  <Subscriber />
+
+  <!-- 
+Message published! start===
+Subscriber.vue:14 Received message: start===
+Subscriber.vue:16 Received message: Hello, world!
+Publisher.vue:13 Message published! end=== 
+-->
+</template>
---
+++ backups/016_slot&组件通信/components/Subscriber.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Receive message: {{ message }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import emitter from '@/utils/emitter';
+import { ref, onMounted, onUnmounted } from 'vue';
+let message = ref('--');
+
+// 绑定事件
+emitter.on('message', (value) => {
+    console.log('Received message:', "start===");
+    message.value = value as string;
+    console.log('Received message:', value);
+});
+onUnmounted(() => {
+    // 解绑事件
+    emitter.off('message');
+    console.log('Event unbound');
+});
+</script>
